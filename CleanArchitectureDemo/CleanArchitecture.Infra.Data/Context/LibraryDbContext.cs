0cbd7fc7356fb295347a928667a296fe52105608
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-08-31 20:31:13 +0600

    Initial commit

---
+++ CleanArchitectureDemo/CleanArchitecture.Infra.Data/Context/LibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
